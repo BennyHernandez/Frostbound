fd8316cc61f9fac065be4ab6d100d8371a903153
Teague <teaguef2010@gmail.com>
2023-12-27 15:13:36 -0800

    Basic Menu created

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,13 +1,18 @@
-extends CanvasLayer
+extends Control
 
-@onready var tween = get_tree().create_tween()
+func _ready():
+	var playButton = $PlayButton
+	var exitButton = $ExitButton
+	playButton.connect("pressed", _on_PlayButton_pressed)
+	exitButton.connect("pressed", _on_ExitButton_pressed)
 
-func appear():
-	tween.interpolate_property(self, "offset:x", 500, 0,
-					0.5, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
-	tween.start()
+func _on_PlayButton_pressed():
+	# Load the game scene
+	var gameScene = preload("res://main.tscn")
+	
+	# Transition to the game scene
+	get_tree().change_scene_to_file("res://main.tscn")
 
-func disappear():
-	tween.interpolate_property(self, "offset:x", 0, 500,
-					0.4, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
-	tween.start()
+func _on_ExitButton_pressed():
+	# Quit the game
+	get_tree().quit()
