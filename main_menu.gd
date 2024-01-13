694f4ef83607ca9b1300b22074548c1eacd592f8
Teague <teaguef2010@gmail.com>
2024-01-13 15:27:43 -0800

    Fixed menu not centering

---
+++ main_menu.gd
@@ -0,0 +1,19 @@
+
+extends Control
+
+func _ready():
+	var playButton = $PlayButton
+	var exitButton = $ExitButton
+	playButton.connect("pressed", _on_PlayButton_pressed)
+	exitButton.connect("pressed", _on_ExitButton_pressed)
+
+func _on_PlayButton_pressed():
+	# Load the game scene
+	var gameScene = preload("res://main.tscn")
+	
+	# Transition to the game scene
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_ExitButton_pressed():
+	# Quit the game
+	get_tree().quit()
